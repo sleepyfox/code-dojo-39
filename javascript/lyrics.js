95ad8a0303040dc5014d96a0f864145112c57cb1
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-16 18:39:12 +0100

    Refactored the lyrics into individual verses

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -1,42 +1,42 @@
-var song = `There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!
+const verse_1 = `There was an old lady who swallowed a fly.
+I don't know why she swallowed a fly - perhaps she'll die!\n\n`
 
-There was an old lady who swallowed a spider;
+const verse_2 = `There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - Perhaps she'll die!\n\n`
 
-There was an old lady who swallowed a bird;
+const verse_3 = `There was an old lady who swallowed a bird;
 How absurd to swallow a bird.
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - Perhaps she'll die!\n\n`
 
-There was an old lady who swallowed a cat;
+const verse_4 = `There was an old lady who swallowed a cat;
 Fancy that to swallow a cat!
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - Perhaps she'll die!\n\n`
 
-There was an old lady that swallowed a dog;
+const verse_5 = `There was an old lady that swallowed a dog;
 What a hog, to swallow a dog!
 She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - Perhaps she'll die!\n\n`
 
-There was an old lady who swallowed a cow;
+const verse_6 = `There was an old lady who swallowed a cow;
 I don't know how she swallowed a cow!
 She swallowed the cow to catch the dog,
 She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - Perhaps she'll die!\n\n`
 
-There was an old lady who swallowed a horse...;
+const verse_7 = `There was an old lady who swallowed a horse...;
 ...She's dead, of course!`
 
-module.exports = song
+module.exports = verse_1 + verse_2 + verse_3 + verse_4 + verse_5 + verse_6 + verse_7
