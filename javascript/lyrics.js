6e194e39053e07b65a2497860c7ca087aa06ab4a
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-16 20:36:45 +0100

    Remove duplicated hard-coded line endings and turn them into logic

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -1,63 +1,65 @@
+const LINE = '\n'
+
 const verse_1 = () => {
-    let opening_line = "There was an old lady who swallowed a fly.\n"
-    let closing_line = "I don't know why she swallowed a fly - perhaps she'll die!\n\n"
-    return opening_line + closing_line
+    let opening_line = "There was an old lady who swallowed a fly."
+    let closing_line = "I don't know why she swallowed a fly - perhaps she'll die!"
+    return [opening_line, closing_line, LINE].join(LINE)
 }
 
 const verse_2 = () => {
-    let opening_line = "There was an old lady who swallowed a spider;\n"
-    let second_line = "That wriggled and wiggled and tickled inside her.\n"
-    let third_line = "She swallowed the spider to catch the fly;\n"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!\n\n"
-    return opening_line + second_line + third_line + closing_line 
+    let opening_line = "There was an old lady who swallowed a spider;"
+    let second_line = "That wriggled and wiggled and tickled inside her."
+    let third_line = "She swallowed the spider to catch the fly;"
+    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
+    return [opening_line, second_line, third_line, closing_line, LINE].join(LINE) 
 }
 
 const verse_3 = () => {
-    let opening_line = "There was an old lady who swallowed a bird;\n"
-    let second_line = "How absurd to swallow a bird.\n"
-    let third_line = "She swallowed the bird to catch the spider,\n"
-    let fourth_line = "She swallowed the spider to catch the fly;\n"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!\n\n"
-    return opening_line + second_line + third_line + fourth_line + closing_line
+    let opening_line = "There was an old lady who swallowed a bird;"
+    let second_line = "How absurd to swallow a bird."
+    let third_line = "She swallowed the bird to catch the spider,"
+    let fourth_line = "She swallowed the spider to catch the fly;"
+    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
+    return [opening_line, second_line, third_line, fourth_line, closing_line, LINE].join(LINE)
 }
 
 const verse_4 = () => {
-    let opening_line = "There was an old lady who swallowed a cat;\n"
-    let second_line = "Fancy that to swallow a cat!\n"
-    let third_line = "She swallowed the cat to catch the bird,\n"
-    let fourth_line = "She swallowed the bird to catch the spider,\n"
-    let fifth_line = "She swallowed the spider to catch the fly;\n"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!\n\n"
-    return opening_line + second_line + third_line + fourth_line + fifth_line + closing_line
+    let opening_line = "There was an old lady who swallowed a cat;"
+    let second_line = "Fancy that to swallow a cat!"
+    let third_line = "She swallowed the cat to catch the bird,"
+    let fourth_line = "She swallowed the bird to catch the spider,"
+    let fifth_line = "She swallowed the spider to catch the fly;"
+    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
+    return [opening_line, second_line, third_line, fourth_line, fifth_line, closing_line, LINE].join(LINE)
 }
 
 const verse_5 = () => {
-    let opening_line = "There was an old lady who swallowed a dog;\n"
-    let second_line = "What a hog, to swallow a dog!\n"
-    let third_line = "She swallowed the dog to catch the cat,\n"
-    let fourth_line = "She swallowed the cat to catch the bird,\n"
-    let fifth_line = "She swallowed the bird to catch the spider,\n"
-    let sixth_line = "She swallowed the spider to catch the fly;\n"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!\n\n"
-    return opening_line + second_line + third_line + fourth_line + fifth_line + sixth_line + closing_line
+    let opening_line = "There was an old lady who swallowed a dog;"
+    let second_line = "What a hog, to swallow a dog!"
+    let third_line = "She swallowed the dog to catch the cat,"
+    let fourth_line = "She swallowed the cat to catch the bird,"
+    let fifth_line = "She swallowed the bird to catch the spider,"
+    let sixth_line = "She swallowed the spider to catch the fly;"
+    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
+    return [opening_line, second_line, third_line, fourth_line, fifth_line, sixth_line, closing_line, LINE].join(LINE)
 }
 
 const verse_6 = () => {
-    let opening_line = "There was an old lady who swallowed a cow;\n"
-    let second_line = "I don't know how she swallowed a cow!\n"
-    let third_line = "She swallowed the cow to catch the dog,\n"
-    let fourth_line = "She swallowed the dog to catch the cat,\n"
-    let fifth_line = "She swallowed the cat to catch the bird,\n"
-    let sixth_line = "She swallowed the bird to catch the spider,\n"
-    let seventh_line = "She swallowed the spider to catch the fly;\n"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!\n\n"
-    return opening_line + second_line + third_line + fourth_line + fifth_line + sixth_line + seventh_line + closing_line
+    let opening_line = "There was an old lady who swallowed a cow;"
+    let second_line = "I don't know how she swallowed a cow!"
+    let third_line = "She swallowed the cow to catch the dog,"
+    let fourth_line = "She swallowed the dog to catch the cat,"
+    let fifth_line = "She swallowed the cat to catch the bird,"
+    let sixth_line = "She swallowed the bird to catch the spider,"
+    let seventh_line = "She swallowed the spider to catch the fly;"
+    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
+    return [opening_line, second_line, third_line, fourth_line, fifth_line, sixth_line, seventh_line, closing_line, LINE].join(LINE)
 }
 
 const verse_7 = () => {
-    let opening_line = "There was an old lady who swallowed a horse...\n"
+    let opening_line = "There was an old lady who swallowed a horse..."
     let closing_line = "...She's dead, of course!"
-    return opening_line + closing_line
+    return [opening_line, closing_line].join(LINE)
 }
 
 module.exports = verse_1() + verse_2() + verse_3() + verse_4() + verse_5() + verse_6() + verse_7()
