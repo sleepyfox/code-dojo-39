81eb0cb30c2efca96ce14eeb111e2978931e20f9
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-16 20:59:50 +0100

    Refector to turn closing line into a function

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -4,24 +4,29 @@ function opening_line(animal){
     return `There was an old lady who swallowed a ${animal}`
 }
 
+function closing_line(options){
+    if (options && options.last){ 
+	return "...She's dead, of course!"
+    } else {
+	return "I don't know why she swallowed a fly - perhaps she'll die!"
+    }
+}
+
 const verse_1 = () => {
-    let closing_line = "I don't know why she swallowed a fly - perhaps she'll die!"
-    return [opening_line('fly.'), closing_line, LINE].join(LINE)
+    return [opening_line('fly.'), closing_line(), LINE].join(LINE)
 }
 
 const verse_2 = () => {
     let second_line = "That wriggled and wiggled and tickled inside her."
     let third_line = "She swallowed the spider to catch the fly;"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
-    return [opening_line('spider;'), second_line, third_line, closing_line, LINE].join(LINE) 
+    return [opening_line('spider;'), second_line, third_line, closing_line(), LINE].join(LINE) 
 }
 
 const verse_3 = () => {
     let second_line = "How absurd to swallow a bird."
     let third_line = "She swallowed the bird to catch the spider,"
     let fourth_line = "She swallowed the spider to catch the fly;"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
-    return [opening_line('bird;'), second_line, third_line, fourth_line, closing_line, LINE].join(LINE)
+    return [opening_line('bird;'), second_line, third_line, fourth_line, closing_line(), LINE].join(LINE)
 }
 
 const verse_4 = () => {
@@ -29,8 +34,7 @@ const verse_4 = () => {
     let third_line = "She swallowed the cat to catch the bird,"
     let fourth_line = "She swallowed the bird to catch the spider,"
     let fifth_line = "She swallowed the spider to catch the fly;"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
-    return [opening_line('cat;'), second_line, third_line, fourth_line, fifth_line, closing_line, LINE].join(LINE)
+    return [opening_line('cat;'), second_line, third_line, fourth_line, fifth_line, closing_line(), LINE].join(LINE)
 }
 
 const verse_5 = () => {
@@ -39,8 +43,7 @@ const verse_5 = () => {
     let fourth_line = "She swallowed the cat to catch the bird,"
     let fifth_line = "She swallowed the bird to catch the spider,"
     let sixth_line = "She swallowed the spider to catch the fly;"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
-    return [opening_line('dog;'), second_line, third_line, fourth_line, fifth_line, sixth_line, closing_line, LINE].join(LINE)
+    return [opening_line('dog;'), second_line, third_line, fourth_line, fifth_line, sixth_line, closing_line(), LINE].join(LINE)
 }
 
 const verse_6 = () => {
@@ -50,13 +53,11 @@ const verse_6 = () => {
     let fifth_line = "She swallowed the cat to catch the bird,"
     let sixth_line = "She swallowed the bird to catch the spider,"
     let seventh_line = "She swallowed the spider to catch the fly;"
-    let closing_line = "I don't know why she swallowed a fly - Perhaps she'll die!"
-    return [opening_line('cow;'), second_line, third_line, fourth_line, fifth_line, sixth_line, seventh_line, closing_line, LINE].join(LINE)
+    return [opening_line('cow;'), second_line, third_line, fourth_line, fifth_line, sixth_line, seventh_line, closing_line(), LINE].join(LINE)
 }
 
 const verse_7 = () => {
-    let closing_line = "...She's dead, of course!"
-    return [opening_line('horse...'), closing_line].join(LINE)
+    return [opening_line('horse...'), closing_line({last: true})].join(LINE)
 }
 
 module.exports = verse_1() + verse_2() + verse_3() + verse_4() + verse_5() + verse_6() + verse_7()
--- javascript/test_song.js
+++ javascript/test_song.js
@@ -7,20 +7,20 @@ I don't know why she swallowed a fly - perhaps she'll die!
 There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - perhaps she'll die!
 
 There was an old lady who swallowed a bird;
 How absurd to swallow a bird.
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - perhaps she'll die!
 
 There was an old lady who swallowed a cat;
 Fancy that to swallow a cat!
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - perhaps she'll die!
 
 There was an old lady who swallowed a dog;
 What a hog, to swallow a dog!
@@ -28,7 +28,7 @@ She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - perhaps she'll die!
 
 There was an old lady who swallowed a cow;
 I don't know how she swallowed a cow!
@@ -37,7 +37,7 @@ She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - Perhaps she'll die!
+I don't know why she swallowed a fly - perhaps she'll die!
 
 There was an old lady who swallowed a horse...
 ...She's dead, of course!`
