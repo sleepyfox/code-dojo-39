2d77c29fcaea80d992c97e73a81aa4ff8579269c
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-17 11:27:03 +0100

    added another test for pairs module

--- javascript/test_pairs.js
+++ javascript/test_pairs.js
@@ -5,3 +5,8 @@ const start = [1,2,3,4,5]
 const target = [[1,2], [2,3], [3,4], [4,5]]
 
 assert.deepEqual(pairs(start), target)
+
+const two = [1,2]
+const result = [[1,2]]
+
+assert.deepEqual(pairs(two), result)
