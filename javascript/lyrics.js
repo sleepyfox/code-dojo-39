ea9f37047a36d64581a9ce3d8360f32546fd7615
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-17 17:47:55 +0100

    refactored to remove the need for logic in closing_line() by using first and last animal exclamations

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -4,32 +4,30 @@ const FIRST = 0
 const SECOND = 1
 
 const Animals = {
-    animals: [ { name: 'horse', exclamation: '' },
-	       { name: 'cow', exclamation: 'I don\'t know how she swallowed a cow!' },
+    animals: [ { name: 'horse', exclamation: "...She's dead, of course!" },
+	       { name: 'cow', exclamation: "I don't know how she swallowed a cow!" },
 	       { name: 'dog', exclamation: 'What a hog, to swallow a dog!' },
 	       { name: 'cat', exclamation: 'Fancy that to swallow a cat!' },
 	       { name: 'bird', exclamation: 'How absurd to swallow a bird.' },
 	       { name: 'spider', exclamation: 'That wriggled and wiggled and tickled inside her.' },
-	       { name: 'fly', exclamation: null } ],
+	       { name: 'fly', exclamation: "I don't know why she swallowed a fly - perhaps she'll die!" } ],
     num_verses: function(){ return this.animals.length },
     last: function(n) {
 	return this.animals.slice(this.animals.length - n, this.animals.length )
     }
 }
 
-console.log(Animals.num_verses())
+function range(n) {
+    return Array(n).fill().map((_, i) => i + 1)
+}
 
 function opening_line(animal, verse = 'middle'){
     var ending = { first: '.', middle: ';', last: '...' }
     return `There was an old lady who swallowed a ${ animal }${ ending[verse] }`
 }
 
-function closing_line(last_line = false){
-    if (last_line){ 
-	return "...She's dead, of course!"
-    } else {
-	return "I don't know why she swallowed a fly - perhaps she'll die!"
-    }
+function closing_line(){
+    return "I don't know why she swallowed a fly - perhaps she'll die!"
 }
 
 function internal_lines(animals){
@@ -39,21 +37,21 @@ function internal_lines(animals){
 function verse(n) {
     let my_animals = Animals.last(n)
     let first_animal = my_animals[FIRST]
-    let second_line = first_animal.exclamation
+
     let core_lines = internal_lines(my_animals)
+
     if (n === 1) {
 	let opening = opening_line(first_animal.name, 'first')
-	return [opening, closing_line(), LINE].join(LINE)
+	return [opening, first_animal.exclamation, LINE].join(LINE)
     } else if (n === Animals.num_verses()) {
 	let opening = opening_line(first_animal.name, 'last')
-	return [opening, closing_line(true)].join(LINE)
+	return [opening, first_animal.exclamation].join(LINE)
     } else {
 	let opening = opening_line(first_animal.name)
-	return [opening, second_line, core_lines, closing_line(), LINE].join(LINE)
+	return [opening, first_animal.exclamation, core_lines, closing_line(), LINE].join(LINE)
     }
 }
 
 module.exports = function () {
-    function range(n) { return Array(n).fill().map((_, i) => i + 1) }
     return range(Animals.num_verses()).map(n => verse(n)).join('')
 }()
