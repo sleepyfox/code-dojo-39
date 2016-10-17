f64c86774060e1c5d12c5f6b26c064fda48f5cb0
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-17 12:19:04 +0100

    refactored to remove hard coded arrays of animals and replace with a slice from a global constant

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -28,39 +28,45 @@ function internal_lines(animals){
     return pairs(animals).map((x) => `She swallowed the ${x[0]} to catch the ${x[1]}`).join(',\n') + ";"
 }
 
-const ANIMALS = [{ name: 'fly'}]
+const ANIMALS = ['horse', 'cow', 'dog', 'cat', 'bird', 'spider', 'fly']
 
 const verse_1 = () => {
-    return [opening_line('fly', 'first'), closing_line(), LINE].join(LINE)
+    let my_animals = ANIMALS.slice(6)
+    return [opening_line(my_animals[0], 'first'), closing_line(), LINE].join(LINE)
 }
 
 const verse_2 = () => {
     let second_line = "That wriggled and wiggled and tickled inside her."
-    let core_line = internal_lines(['spider', 'fly'])
-    return [opening_line('spider'), second_line, core_line, closing_line(), LINE].join(LINE) 
+    let my_animals = ANIMALS.slice(5)
+    let core_line = internal_lines(my_animals)
+    return [opening_line(my_animals[0]), second_line, core_line, closing_line(), LINE].join(LINE) 
 }
 
 const verse_3 = () => {
     let second_line = "How absurd to swallow a bird."
-    let core_lines = internal_lines(['bird', 'spider', 'fly'])
+    let my_animals = ANIMALS.slice(4)
+    let core_lines = internal_lines(my_animals)
     return [opening_line('bird'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_4 = () => {
     let second_line = "Fancy that to swallow a cat!"
-    let core_lines = internal_lines(['cat', 'bird', 'spider', 'fly'])
+    let my_animals = ANIMALS.slice(3)
+    let core_lines = internal_lines(my_animals)
     return [opening_line('cat'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_5 = () => {
     let second_line = "What a hog, to swallow a dog!"
-    let core_lines = internal_lines(['dog', 'cat', 'bird', 'spider', 'fly'])
+    let my_animals = ANIMALS.slice(2)
+    let core_lines = internal_lines(my_animals)
     return [opening_line('dog'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_6 = () => {
     let second_line = "I don't know how she swallowed a cow!"
-    let core_lines = internal_lines(['cow', 'dog', 'cat', 'bird', 'spider', 'fly'])
+    let my_animals = ANIMALS.slice(1)
+    let core_lines = internal_lines(my_animals)
     return [opening_line('cow'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
