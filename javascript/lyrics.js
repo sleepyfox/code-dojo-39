f861f5306d489dd2e0f3f097c4d0ae2fe2ad9e04
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-10-17 11:44:37 +0100

    refactor to use pairs() to generate internal lines of each verse

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -1,3 +1,4 @@
+pairs = require('./pairs')
 const LINE = '\n'
 
 function opening_line(animal){
@@ -12,48 +13,42 @@ function closing_line(options){
     }
 }
 
+function internal_lines(animals){
+    return pairs(animals).map((x) => `She swallowed the ${x[0]} to catch the ${x[1]}`).join(',\n') + ";"
+}
+
 const verse_1 = () => {
     return [opening_line('fly.'), closing_line(), LINE].join(LINE)
 }
 
 const verse_2 = () => {
     let second_line = "That wriggled and wiggled and tickled inside her."
-    let third_line = "She swallowed the spider to catch the fly;"
-    return [opening_line('spider;'), second_line, third_line, closing_line(), LINE].join(LINE) 
+    let core_line = internal_lines(['spider', 'fly'])
+    return [opening_line('spider;'), second_line, core_line, closing_line(), LINE].join(LINE) 
 }
 
 const verse_3 = () => {
     let second_line = "How absurd to swallow a bird."
-    let third_line = "She swallowed the bird to catch the spider,"
-    let fourth_line = "She swallowed the spider to catch the fly;"
-    return [opening_line('bird;'), second_line, third_line, fourth_line, closing_line(), LINE].join(LINE)
+    let core_lines = internal_lines(['bird', 'spider', 'fly'])
+    return [opening_line('bird;'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_4 = () => {
     let second_line = "Fancy that to swallow a cat!"
-    let third_line = "She swallowed the cat to catch the bird,"
-    let fourth_line = "She swallowed the bird to catch the spider,"
-    let fifth_line = "She swallowed the spider to catch the fly;"
-    return [opening_line('cat;'), second_line, third_line, fourth_line, fifth_line, closing_line(), LINE].join(LINE)
+    let core_lines = internal_lines(['cat', 'bird', 'spider', 'fly'])
+    return [opening_line('cat;'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_5 = () => {
     let second_line = "What a hog, to swallow a dog!"
-    let third_line = "She swallowed the dog to catch the cat,"
-    let fourth_line = "She swallowed the cat to catch the bird,"
-    let fifth_line = "She swallowed the bird to catch the spider,"
-    let sixth_line = "She swallowed the spider to catch the fly;"
-    return [opening_line('dog;'), second_line, third_line, fourth_line, fifth_line, sixth_line, closing_line(), LINE].join(LINE)
+    let core_lines = internal_lines(['dog', 'cat', 'bird', 'spider', 'fly'])
+    return [opening_line('dog;'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_6 = () => {
     let second_line = "I don't know how she swallowed a cow!"
-    let third_line = "She swallowed the cow to catch the dog,"
-    let fourth_line = "She swallowed the dog to catch the cat,"
-    let fifth_line = "She swallowed the cat to catch the bird,"
-    let sixth_line = "She swallowed the bird to catch the spider,"
-    let seventh_line = "She swallowed the spider to catch the fly;"
-    return [opening_line('cow;'), second_line, third_line, fourth_line, fifth_line, sixth_line, seventh_line, closing_line(), LINE].join(LINE)
+    let core_lines = internal_lines(['cow', 'dog', 'cat', 'bird', 'spider', 'fly'])
+    return [opening_line('cow;'), second_line, core_lines, closing_line(), LINE].join(LINE)
 }
 
 const verse_7 = () => {
