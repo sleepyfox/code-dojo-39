a907fb7654fdae566e1aa6a6456f90d6a2a2b4f9
Egga (Jan Hartung) <github@egga.de>
2016-12-12 12:05:25 +0100

    add java version

---
+++ java/Song.java
@@ -0,0 +1,48 @@
+class Song {
+
+    public static void main(String[] args) {
+
+        String song = "There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a spider;\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a bird;\n" +
+                "How absurd to swallow a bird.\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a cat;\n" +
+                "Fancy that to swallow a cat!\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a dog;\n" +
+                "What a hog, to swallow a dog!\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a cow;\n" +
+                "I don't know how she swallowed a cow!\n" +
+                "She swallowed the cow to catch the dog,\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a horse...\n" +
+                "...She's dead, of course!";
+
+        System.out.println(song);
+    }
+}
